32d2a92f8abac6a24748b9cd7bc7d1b978db627e
Sandi <sandi.c.gau@gmail.com>
2018-10-03 23:04:59 -0500

    more changes

--- server.js
+++ server.js
@@ -4,8 +4,5 @@ const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, '/dist')));
 
-app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/dist/'))
-  })
 const port = process.env.PORT || 8080;
 app.listen(port);
