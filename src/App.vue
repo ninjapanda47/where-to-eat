8391143ebb1375e8759fbd34d8d2e2255c9a9bf3
Sandi <sandi.c.gau@gmail.com>
2019-02-02 21:08:27 -0600

    Added background and cleared up logic

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,22 @@
             title="Where to eat?"
           >
             <Main v-on:submit="getRestaurants"></Main>
+            <br>
+            <span>Photo credit to</span>
+            <a
+              style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px"
+              href="https://unsplash.com/@therachelstory?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Download free do whatever you want high-resolution photos from Rachel Park"
+            ><span style="display:inline-block;padding:2px 3px"><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white"
+                  viewBox="0 0 32 32"
+                >
+                  <title>unsplash-logo</title>
+                  <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
+                </svg></span><span style="display:inline-block;padding:2px 3px">Rachel Park</span></a>
           </b-card>
           <Modal
             ref="modal"
@@ -58,10 +74,10 @@ export default {
         this.$data.num--;
         this.$refs.modal.show();
       } else {
-        this.$data.num = 50;
         api.getRestaurants(location).then(res => {
           this.$data.location = location;
           this.$data.restaurants = res;
+          this.$data.num = res.length;
           this.$data.restaurant = this.$data.restaurants[x];
           this.$data.restaurants.splice(x, 1);
           this.$data.num--;
